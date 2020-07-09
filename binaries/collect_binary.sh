fbcf958999aedfeed68fc49e9cf33cab4004bee5
Michael Conard <maconard@mtu.edu>
2020-07-08 21:33:48 -0400

    compilation support script

---
+++ binaries/collect_binary.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Build, name, and sort RIOT binaries
+# helps speed up the recompiling process
+
+# Note: T1 and T2 should be entered in seconds
+
+# usage: ./collect_binary <board> <master|worker> <topo|K> <T1> <T2>
+
+BOARD="$1"
+TYPE="$2"
+PARAM="$3"
+T1="$4"
+T2="$5"
+
+if [[ "$BOARD" != "iotlab-m3" && "$BOARD" != "native" ]]; then
+    echo "Please select either iotlab-m3 or native."
+    echo "Usage: ./collect_binary <board> <master|worker> <topo|K> <T1> <T2>"
+    exit
+fi
+
+if [[ "$TYPE" != "master" && "$TYPE" != "worker" ]]; then
+    echo "Please select either master or worker."
+    echo "Usage: ./collect_binary <board> <master|worker> <topo|K> <T1> <T2>"
+    exit
+fi
+
+if [[ "$PARAM" == "" ]]; then
+    echo "Please enter either a master topology or worker K"
+    echo "Usage: ./collect_binary <board> <master|worker> <topo|K> <T1> <T2>"
+    exit
+fi
+
+if [[ "$TYPE" == "worker" && "$T1" == "" ]]; then
+    echo "Please enter a T1 value in seconds."
+    echo "Usage: ./collect_binary <board> <master|worker> <topo|K> <T1> <T2>"
+    exit
+fi
+
+if [[ "$TYPE" == "worker" && "$T2" == "" ]]; then
+    echo "Please enter a T1 value in milliseconds."
+    echo "Usage: ./collect_binary <board> <master|worker> <topo|K> <T1> <T2>"
+    exit
+fi
+
+if [[ "$TYPE" == "master" ]]; then
+    FILE="master_${BOARD}_${PARAM}.elf"
+    TARGET="./${BOARD}/${TYPE}/$FILE"
+
+    echo "Compiling $FILE..."
+    pushd ../cpsiot_masternode > /dev/null
+        make BOARD="$BOARD" TOPO="$PARAM" > /dev/null
+    popd  > /dev/null
+    cp "../cpsiot_masternode/bin/${BOARD}/master_node.elf" "$TARGET"
+    echo "Saved the master binary $FILE."
+else
+    FILE="worker_${BOARD}_${PARAM}-${T1}-${T2}.elf"
+    TARGET="./${BOARD}/${TYPE}/$FILE"
+    T1us=$(expr $T1 \* 1000000)
+    T2us=$(expr $T2 \* 1000000)
+
+    echo "Compiling $FILE..."
+    pushd ../cpsiot_workernode > /dev/null
+        make BOARD="$BOARD" K="$PARAM" T1="$T1us" T2="$T2us" > /dev/null
+    popd  > /dev/null
+    cp "../cpsiot_workernode/bin/${BOARD}/worker_node.elf" "$TARGET"
+    echo "Saved the worker binary $FILE."
+fi
+
